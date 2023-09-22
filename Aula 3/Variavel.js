a84931b9db5b1da76e0515a36ff0b290906a8aa7
CavMCarolina <RM552925@fiap.com.br>
2023-09-22 11:09:13 -0300

    Aulas

---
+++ Aula 3/Variavel.js
@@ -0,0 +1,12 @@
+// valor indefinido
+let nome;
+
+// vazia
+let disciplina = null;
+
+// valor definido
+let texto = "Exemplo";
+
+console.log(nome);
+console.log(disciplina);
+console.log (texto);
