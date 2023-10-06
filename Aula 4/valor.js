1ff6ff015cb3fa2c4c1b8df5149b8d1ff04c8ee8
CavMCarolina <RM552925@fiap.com.br>
2023-10-06 11:15:59 -0300

    valores

--- Aula 4/valor.js
+++ Aula 4/valor.js
@@ -1,7 +1,7 @@
 let valor = 5;
 
 if (valor > 7){
-    console.log("O valor é maior que sete!")
+    console.log("O valor é maior que sete!") // não é necessário ; para fechar quando já tem o }
 } 
 else {
     console.log("O valor é menor que sete")
