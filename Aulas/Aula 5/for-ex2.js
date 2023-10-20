329ac4424531ac66e3d652737bfb8b9a278e3270
CavMCarolina <RM552925@fiap.com.br>
2023-10-20 11:10:33 -0300

    Exemplos for

---
+++ Aulas/Aula 5/for-ex2.js
@@ -0,0 +1,6 @@
+// const = variável para ser usada no for
+const frutas = ["Maçã", "Banana", "Uva", "Pera"]
+
+for (let i = 0; i < frutas.length; i ++) {
+    console.log(`${i} - ${frutas[i]}`);
+}
---
+++ Aulas/Aula 5/for-ex3.js
@@ -0,0 +1,6 @@
+const frutas = ["Maçã", "Banana", "Uva", "Pera"]
+
+// for of = for in (python)
+for (let fruta of frutas) {
+    console.log(fruta);
+}
