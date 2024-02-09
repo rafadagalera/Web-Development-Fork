1d889cefe81b3da43e29195f376af28f3c4421a3
CavMCarolina <RM552925@fiap.com.br>
2024-02-09 11:45:09 -0300

    Atualização CP2

---
+++ Checkpoints/Checkpoint 2/animals.js
@@ -0,0 +1,8 @@
+const animals = ['arara', 'macaco', 'hamster', 'coala', 'panda', 'jaguatirica'];
+
+if (animals.includes('leão') != true) {
+    console.log("Não existe um leão no array 'animals'")
+}
+else {
+    console.log("Existe um leão no array 'animals'")
+}
---
+++ Checkpoints/Checkpoint 2/series.js
@@ -0,0 +1,8 @@
+const my3FavoriteTVShows = ['Arcane', 'Bungo Stray Dogs', 'Tokyo Ghoul'];
+
+if (my3FavoriteTVShows.length === 3) {
+    console.log(true)
+} 
+else {
+    console.log(false)
+}
