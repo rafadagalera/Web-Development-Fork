c5864927f184d4e59a78add8fff8ce749853845f
CavMCarolina <RM552925@fiap.com.br>
2024-02-23 11:52:53 -0300

    Lista aula 2

--- Aulas/Aula 2/aula2.js
+++ Aulas/Aula 2/aula2.js
@@ -1,9 +1,11 @@
 const btn1 = document.querySelector('button')
 const listaFilmes = document.querySelector('#listaFilmes')
 const itemLista = document.createElement('li')
+const textoUsuario = document.querySelector('#filmeInput')
 
 btn1.addEventListener('click', ()=>{
     listaFilmes.append(itemLista)
     // Adiciona um texto dentro do html
-    itemLista.innerHTML = '<strong>Harry Potter</strong>'
+    itemLista.innerHTML = textoUsuario.value
+    itemLista.style.backgroundColor = "red"
 })
